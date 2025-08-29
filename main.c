96c73b9e15283776cc9a02b4567427055fa1632a
Kasey Clarke <KaseyDClarke@gmail.com>
2025-08-29 13:04:43 -0700

    constructor and destructor added

--- main.c
+++ main.c
@@ -1,36 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct potion_s
 {
-    char* name;
+    char *name;
     void (*print_name)(struct potion_s *);
-    
+
     int ounces;
     void (*print_ounces)(struct potion_s *);
 
 } potion_t;
 
-void print_name(potion_t* potion)
+static void print_name(potion_t *p)
 {
-    printf("%s\n", potion->name);
+    printf("%s\n", p->name);
 }
 
-void print_ounces(potion_t* potion)
+static void print_ounces(potion_t *p)
 {
-    printf("%d ounces\n", potion->ounces);
+    printf("%d ounces\n", p->ounces);
 }
 
-int main()
+char * copy_string(const char *src)
+{
+    size_t len = strlen(src) + 1;      // +1 for null terminator
+    char * dst = malloc(len);
+    if (dst != NULL)
+    {
+        memcpy(dst, src, len);
+    }
+    return dst;
+}
+
+potion_t create_potion(const char *name, const int ounces)
 {
     potion_t potion;
-    
-    potion.name = "matcha";
+
+    potion.name = copy_string(name);
     potion.print_name = print_name;
-   
-    potion.ounces = 12;
+    
+    potion.ounces = ounces;
     potion.print_ounces = print_ounces;
 
-    potion.print_name(&potion);
-    potion.print_ounces(&potion);
-    
+    return potion;
+}
+
+void destroy_potion(potion_t * potion)
+{
+    if (potion->name != NULL)
+    {
+        free(potion->name);
+        potion->name = NULL;
+    }
+}
+
+int main(void)
+{
+    // Create a potion
+    potion_t matcha = create_potion("matcha", 12);
+
+    // Call its "methods"
+    matcha.print_name(&matcha);
+    matcha.print_ounces(&matcha);
+
+    // Clean up
+    destroy_potion(&matcha);
+
+    return 0;
 }
